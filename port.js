29f0c8fb9ea78edee6ab31ac90ca27f45b4c251f
dominicmena <mena.dominic@gmail.com>
2023-02-11 22:10:52 -0600

    added the pagination and autoplay

--- port.js
+++ port.js
@@ -1,4 +1,15 @@
-const navbar = document.querySelector("navbar");
-window.addEventListener("scroll", function() {
-    navbar.classList.toggle("sticky", window.scrollY >100)
-});
+// const navbar = document.querySelector("navbar");
+// window.addEventListener("scroll", function() {
+//     navbar.classList.toggle("sticky", window.scrollY >100)
+// });
+
+// const swiper = new Swiper('.swiper', {
+//     loop: true,
+//     pagination: {
+//       el: '.swiper-pagination',
+//     },
+//     navigation: {
+//       nextEl: '.swiper-button-next',
+//       prevEl: '.swiper-button-prev',
+//     },
+//   });
