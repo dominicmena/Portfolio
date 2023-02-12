1692eea1b053968fb7b4829fc57db9071cdc805f
dominicmena <mena.dominic@gmail.com>
2023-02-12 07:21:56 -0600

    trying to figure out placement of dropdown menu

--- port.js
+++ port.js
@@ -1,18 +1,18 @@
-// const navbar = document.querySelector("navbar");
+// const header = document.querySelector("header");
 // window.addEventListener("scroll", function() {
-//     navbar.classList.toggle("sticky", window.scrollY >100)
+//     header.classList.toggle("sticky", window.scrollY >100)
 // });
 
 let menu = document.querySelector("#menu-icon");
-let navbar = document.querySelector(".navbar");
+let navlist = document.querySelector(".navbar");
 
 menu.onclick = () => {
     menu.classList.toggle('bx-x');
-    navbar.classList.toggle('bx-x');
+    navlist.classList.toggle('bx-x');
 }
 window.onscroll = () => {
     menu.classList.remove('bx-x');
-    navbar.classList.remove('bx-x');
+    navlist.classList.remove('bx-x');
 }
 // const swiper = new Swiper('.swiper', {
 //     loop: true,
